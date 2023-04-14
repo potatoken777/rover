e55ea02726e11220ce96f6c7222b8db60276b7e8
potatoken777 <122539617+potatoken777@users.noreply.github.com>
2023-04-14 08:45:15 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 input.onButtonPressed(Button.A, function () {
     basic.pause(500)
-    wuKong.setAllMotor(100, 0)
+    wuKong.setAllMotor(100, 20)
     basic.pause(1000)
     wuKong.stopAllMotor()
 })
@@ -12,7 +12,7 @@ input.onButtonPressed(Button.AB, function () {
 })
 input.onButtonPressed(Button.B, function () {
     basic.pause(500)
-    wuKong.setAllMotor(0, 100)
+    wuKong.setAllMotor(20, 100)
     basic.pause(1000)
     wuKong.stopAllMotor()
 })
